f92cf691624e7a51384376c1df659051994d407b
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-01-14 21:56:57 +0000

    FIX TORRENT use ascii

---
+++ Torrent.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.*;
+import java.io.IOException;
+
+public class Torrent{
+    private Message info;
+
+    public Torrent(String filename) {
+        Path p = Paths.get("../interview.torrent");
+        byte[] b = new byte[0];
+        try {
+            b = Files.readAllBytes(p);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String s = new String(b, StandardCharsets.US_ASCII);
+        info = new Message(s);
+    }
+
+    public Message getInfo(){
+        return info;
+    }
+
+    public static void main(String[] args){
+        Torrent t = new Torrent("[kickass.so]the.interview.2014.1080p.5.1.dd.custom.nl.subs.unlimitedmovies.torrent");
+        System.out.println(t.getInfo());
+    }
+}
