5f49388b5801d5519c32385a2249936db33dfc24
Fishelstix <Max@Maxwell-Fishelsons-27-iMac-i5.local>
2015-01-14 21:15:25 -0500

    ¯\_(ツ)_/¯

--- Torrent.java
+++ Torrent.java
@@ -3,12 +3,14 @@ import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.charset.StandardCharsets;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 public class Torrent{
-    private Message info;
+    private Message metainfo;
+    private String bencodedInfo;
 
     public Torrent(String filename) {
-        Path p = Paths.get("../interview.torrent");
+        Path p = Paths.get(filename);
         byte[] b = new byte[0];
         try {
             b = Files.readAllBytes(p);
@@ -16,15 +18,21 @@ public class Torrent{
             e.printStackTrace();
         }
         String s = new String(b, StandardCharsets.US_ASCII);
-        info = new Message(s);
+        metainfo = new Message(s);
     }
 
-    public Message getInfo(){
-        return info;
+    public String trackerRequest(){
+	String url = new String(((String)metainfo.get("announce"))+"?");
+	
+	return url;
+    }
+
+    public Message getMetainfo(){
+        return metainfo;
     }
 
     public static void main(String[] args){
         Torrent t = new Torrent("[kickass.so]the.interview.2014.1080p.5.1.dd.custom.nl.subs.unlimitedmovies.torrent");
-        System.out.println(t.getInfo());
+        System.out.println(t.getMetainfo());
     }
 }
