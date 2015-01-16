328a522d0324083aa235fe6e32dd9254709e0f73
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-01-16 20:29:01 +0000

    url percent encode bytes for sha1 option

--- SHA1.java
+++ SHA1.java
@@ -20,14 +20,24 @@ public class SHA1 {
         return digest(s.getBytes());
     }
 
-    public String digest(byte[] bytes) {
-        return byteArray2Hex(md.digest(bytes));
+    public String digest(String s, boolean url) {
+        return digest(s.getBytes(), url);
     }
 
-    private static String byteArray2Hex(byte[] hash) {
+    public String digest(byte[] bytes) {
+        return digest(bytes, false);
+    }
+
+    public String digest(byte[] bytes, boolean url) {
+        byte[] digest = md.digest(bytes);
+        return byteArray2Hex(digest, url);
+    }
+
+    private static String byteArray2Hex(byte[] hash, boolean url) {
+        String base = url ? "%%" : new String();
         Formatter formatter = new Formatter();
         for (byte b : hash) {
-            formatter.format("%02x", b);
+            formatter.format(base+"%02x", b);
         }
         return formatter.toString();
     }
