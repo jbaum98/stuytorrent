f804748a1816f2fac612fd4b88b1b39b42650ca9
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-02-12 04:39:24 +0000

    cleanup and comment Interval

--- Interval.java
+++ Interval.java
@@ -1,5 +1,13 @@
 import java.util.TreeSet;
-import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * represents a list of length locations from 0 to length-1,
+ * where each location has a status.
+ * Avoids storing data on each location by instead
+ * storing a {@link Set} of {@link Boundary}>s where
+ * the status of a given location is determined by the
+ * first {@link Boundary} strictly above that location.
+ */
 
 public class Interval {
     private TreeSet<Boundary> boundaries;
@@ -8,47 +16,44 @@ public class Interval {
         this(length,Status.ABSENT);
     }
 
+    /**
+     * creates a {@link Boundary} at length
+     * because the highest boundary determines the length
+     */
     public Interval(int length, Status defaultStatus) {
         boundaries = new TreeSet<Boundary>();
         boundaries.add(new Boundary(length, defaultStatus));
     }
 
     public int length() {
-        return lastBoundary().location;
+        return boundaries.last().location;
     }
 
-    private Boundary lastBoundary() {
-        return boundaries.last();
-    }
-
-    public Status status(int location) {
-        return higher(location).status();
-    }
-
-    public boolean is(int location, Status status) {
-        return status(location) == status;
-    }
-
+    /** {@see TreeSet#higher} */
     private Boundary higher(int i) {
         return boundaries.higher(new Boundary(i));
     }
 
-    private Boundary lower(int i) {
-        return boundaries.lower(new Boundary(i));
-    }
-
+    /** {@see TreeSet#ceiling} */
     private Boundary ceiling(int i) {
         return boundaries.ceiling(new Boundary(i));
     }
 
+    /** {@see TreeSet#floor} */
     private Boundary floor(int i) {
         return boundaries.floor(new Boundary(i));
     }
 
-    private boolean hasBoundaryAt(int i) {
-        return boundaries.contains(new Boundary(i));
+    public Status statusAt(int location) {
+        return higher(location).status();
     }
 
+    /**
+     * Set all locations in the interval [lo, hi) to status.
+     * Similar to slice notation in that hi is not included,
+     * and that the maximum value for hi is length even though
+     * the highest location is length-1
+     */
     public void set(int lo, int hi, Status status) {
         if ( 0 > lo || lo > hi || hi >= length() ) return; // sanity checks
         Boundary low = addOrFetchBelow(lo, status);
@@ -56,17 +61,24 @@ public class Interval {
         clearBetween(low, high);
     }
 
+    /**
+     * Creates a boundary with status at location,
+     * unless that location is already of the correct status
+     * in which case we return the higher boundary of the same status.
+     * @param location location of the boundary
+     * @param status   status of the boundary
+     * @return a Boundary at or higher than location with status
+     */
     private Boundary addOrFetchAbove(int location, Status status) {
         Boundary above = ceiling(location);
         if (above.is(status)) {
             return above;
-        } else if (above.location == location) {
+        } else if (above.location == location) {                // there exists a boundary at location of incorrect status
             Boundary next_above = boundaries.higher(above);
-            if (next_above != null && next_above.is(status)) {
-                boundaries.remove(above);
-                return next_above;
-            } else {
-                above.setStatus(status);
+            if (next_above != null && next_above.is(status)) {  // if there is a correct and higher boundary
+                return next_above;                              // above will be deleted later in clearBetween
+            } else {                                            // there is no higher correct boundary
+                above.setStatus(status);                        // modify existing boundary
                 return above;
             }
         } else {
@@ -74,8 +86,16 @@ public class Interval {
         }
     }
 
+    /**
+     * creates a boundary at location, preserving it's status
+     * unless that location is already of the new status,
+     * in which case we return the next lowest boundary.
+     * @param location location of the boundary
+     * @param status   status we want to set the locations above this
+     * @return a Boundary at or lower than location
+     */
     private Boundary addOrFetchBelow(int location, Status status) {
-        if (status(location) == status) {
+        if (statusAt(location) == status) {
             return floor(location); // might be null
         } else {
             return addBoundary(location);
@@ -88,19 +108,8 @@ public class Interval {
             boundaries.remove(above);
         }
     }
-
     private Boundary above(Boundary b) {
         return boundaries.higher(b);
-    }
-
-    /**
-     * add a boundary at location of the same status as location's current status
-     * @param location should be greater than 0
-     */
-    private Boundary addBoundary(int location) {
-        Boundary b = new Boundary(location, status(location));
-        boundaries.add(b);
-        return b;
     }
 
     /**
@@ -114,7 +123,17 @@ public class Interval {
         return b;
     }
 
-    public Tuple getAbsentInterval(int max_size) {
+    /**
+     * add a boundary at location of the same status as location's current status
+     * @param location should be greater than 0
+     */
+    private Boundary addBoundary(int location) {
+        Boundary b = new Boundary(location, statusAt(location));
+        boundaries.add(b);
+        return b;
+    }
+
+    /*public Tuple getAbsentInterval(int max_size) {
         for (Boundary b : boundaries) {
             if (b.is(Status.ABSENT)) {
                 Boundary below = boundaries.lower(b);
@@ -128,12 +147,12 @@ public class Interval {
             }
         }
         return null;
-    }
+    }*/
 
     public String toString() {
         String out = new String("<");
         for (int i = 0; i < length(); i++) {
-            out += status(i).c;
+            out += statusAt(i);
         }
         return out+">";
     }
@@ -150,9 +169,6 @@ public class Interval {
         System.out.println(i);
         i.set(40,80, Status.PRESENT);
         System.out.println(i);
-        System.out.println("30: " + i.is(30, Status.PRESENT));
-        System.out.println("70: " + i.is(70, Status.PRESENT));
-        System.out.println("80: " + i.is(80, Status.PRESENT));
         i.set(40,60, Status.PRESENT);
         System.out.println(i);
         System.out.println("//////////////////////");
@@ -166,9 +182,14 @@ public class Interval {
     }
 }
 
+/**
+ * represents a boundary such that all locations below the boundary
+ * have a status set by the boundary
+ */
+
 class Boundary implements Comparable<Boundary> {
     public final int location;
-    private final AtomicReference<Status> status;
+    private Status status;
 
     public Boundary(int location) {
         this(location, Status.ABSENT);
@@ -176,35 +197,46 @@ class Boundary implements Comparable<Boundary> {
 
     public Boundary(int location, Status status) {
         this.location = location;
-        this.status = new AtomicReference<Status>(status);
+        this.status = status;
     }
 
     public Status status() {
-        return status.get();
+        return status;
     }
 
-    public void setStatus(Status newStatus) {
-        status.set(newStatus);
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     public boolean is(Status status) {
         return this.status() == status;
     }
 
+    /**
+     * Comparison depends only on location
+     * and not on status, so that {@link Interval#boundaries}
+     * orders correctly
+     */
     public int compareTo(Boundary other) {
         return this.location - other.location;
     }
 
+    /**
+     * Equality depends only on location
+     * and not on status, so that {@link Interval#boundaries}
+     * orders correctly
+     */
     public boolean equals(Boundary other) {
         return this.location == other.location;
     }
 
+    /**
+     * Equality depends only on location
+     * and not on status, so that {@link Interval#boundaries}
+     * orders correctly
+     */
     public int hashCode() {
         return location;
-    }
-
-    public boolean strictEquals(Boundary other) {
-        return this.location == other.location && this.status == other.status;
     }
 
     public String toString() {
@@ -212,11 +244,16 @@ class Boundary implements Comparable<Boundary> {
     }
 }
 
+/**
+ * represents the status of a location
+ */
+
 enum Status {
     ABSENT('_'),
     PRESENT('*'),
     PENDING('?');
 
+    /** the character used to represent this status */
     public final char c;
     public String toString() { return ""+c;}
 
