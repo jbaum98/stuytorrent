7003b0a09544e495b5e89ef8d5ebf434f211b468
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-02-15 18:41:10 +0000

    use int for piece_length

--- Info.java
+++ Info.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Arrays;
 
 public abstract class Info {
-    public abstract long piece_length();
+    public abstract int piece_length();
     public abstract byte[]    pieces();
     public abstract String    name();
     public abstract long       length();
@@ -10,22 +10,30 @@ public abstract class Info {
     public byte[] hash(int piece_index) {
         return Arrays.copyOfRange(pieces(), piece_index*20, (piece_index-1)*20);
     }
+
+    public int num_pieces() {
+        return (int) (length() / piece_length() + 1);
+    }
+
+    public int overflow() {
+        return (int) (length() % piece_length());
+    }
 }
 
 class InfoSingle extends Info {
-    private final long piece_length;
+    private final int piece_length;
     private final byte[]    pieces;
     private final String    name;
     private final long      length;
 
     public InfoSingle(HashMap<String, BencodingObj> map) {
-        piece_length = (long) map.get("piece length").value;
+        piece_length = (int) map.get("piece length").value;
         pieces = ((String) map.get("pieces").value).getBytes(Globals.CHARSET);
         name = (String) map.get("name").value;
         length = (long) map.get("length").value;
     }
 
-    public long piece_length() {
+    public int piece_length() {
         return piece_length;
     }
 
