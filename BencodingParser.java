a93f05b49e2260228dc7cdbb841e5237457d20ac
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-02-15 16:58:33 +0000

    finish reworking BP

--- BencodingParser.java
+++ BencodingParser.java
@@ -8,12 +8,38 @@ import java.util.HashMap;
 public class BencodingParser {
     private String data;
     /** current position */
-    private int counter;
+    private int index;
 
-    public HashMap parse(String s) {
+    public BencodingObj parse(String s) {
         this.data = s;
-        this.counter = 0;
-        return d();
+        this.index = 0;
+        ArrayList<BencodingObj> out = new ArrayList<BencodingObj>();
+        parse(out);
+        if (out.size() == 1) {
+            return out.get(0);
+        } else {
+            return new BencodingObj<ArrayList<BencodingObj>>(out, data);
+        }
+    }
+
+    /**
+     * used to parse lists and hashes
+     */
+    private void parse(ArrayList<BencodingObj> out) {
+        char c;
+        while (index < data.length() && (c = data.charAt(index)) != 'e'){
+            if (c == 'i') {
+                i(out);
+            } else if (isDigit(c)) {
+                s(out);
+            } else if (c == 'l') {
+                l(out);
+            } else if (c == 'd') {
+                d(out);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     private boolean isDigit(char c) {
@@ -25,67 +51,61 @@ public class BencodingParser {
      * letter i used for integer, but we sometimes need to parse ints &gt; 2^31
      * so we use longs
      */
-    private long i() {
-        int start = counter;
-        int end = data.indexOf('e', counter);
-        counter = end + 1;
-        return Long.parseLong(data.substring(start+1,end));
+    private void i(ArrayList<BencodingObj> out) {
+        int end = data.indexOf('e', index);
+        long value = Long.parseLong(data.substring(index+1,end));
+        String original = data.substring(index,end+1);
+        out.add(new BencodingObj<Long>(value, original));
+        index = end + 1;
     }
 
     /**
      * parse a string
      */
-    private String s() {
-        int colonIndex = data.indexOf(':', counter);
-        int length = Integer.parseInt(data.substring(counter,colonIndex));
-        int end = colonIndex + length + 1; // one after last char
-        counter = end;
-        return data.substring(colonIndex+1,end);
+    private void s(ArrayList<BencodingObj> out) {
+        int colonIndex = data.indexOf(':', index);
+        int length = Integer.parseInt(data.substring(index,colonIndex));
+        int end = colonIndex + length;
+        String value = data.substring(colonIndex+1,end+1);
+        String original = data.substring(index, end+1);
+        out.add(new BencodingObj<String>(value, original));
+        index = end + 1;
     }
 
-    /**
-     * used to parse lists and hashes
-     */
-    private ArrayList many() {
-        ArrayList out = new ArrayList();
-        char c = data.charAt(counter);
-        while (c != 'e'){
-            if (c == 'i') {
-                out.add(i());
-            } else if (isDigit(c)) {
-                out.add(s());
-            } else if (c == 'l') {
-                out.add(l());
-            } else if (c == 'd') {
-                out.add(d());
-            } else {
-                throw new IllegalArgumentException();
-            }
-            c = data.charAt(counter);
-        }
-        counter++;
-        return out;
-    }
 
     /**
      * parse a list
      */
-    private ArrayList l() {
-        counter++; //get past l
-        return many();
+    private void l(ArrayList<BencodingObj> out) {
+        int start = index;
+        index++; //get past l
+        ArrayList<BencodingObj> me = new ArrayList<BencodingObj>();
+        parse(me);
+        out.add(new BencodingObj<ArrayList<BencodingObj>>(me,data.substring(start,index+1)));
+        index++; //get past e
     }
 
     /**
      * parse a dictionary
      */
-    private HashMap d() {
-        counter++; //get past d
-        ArrayList parsed = many(); // get a list of elements
-        HashMap out = new HashMap();
-        for (int i = 0; i < parsed.size(); i+=2) {
-            out.put(parsed.get(i), parsed.get(i+1)); // every other element is a key or a value
+    private void d(ArrayList<BencodingObj> out) {
+        int start = index;
+        index++; //get past d
+        ArrayList<BencodingObj> me = new ArrayList<BencodingObj>();
+        parse(me);
+        HashMap<BencodingObj<String>, BencodingObj> map = new HashMap<BencodingObj<String>, BencodingObj>();
+        for (int i = 0; i + 1 < me.size(); i+=2) {
+            map.put(me.get(i), me.get(i+1)); // every other element is a key or a value
         }
-        return out;
+        out.add(new BencodingObj<HashMap<BencodingObj<String>, BencodingObj>>(map,data.substring(start,index+1)));
+        index++; //get past e
+    }
+
+    public static void main(String[] args) {
+        BencodingParser bp = new BencodingParser();
+        BencodingObj x = bp.parse("d4:thislllli3eeeeee");
+        System.out.println(x);
+        System.out.println(x.original);
     }
 }
 
@@ -96,5 +116,9 @@ class BencodingObj<T> {
     public BencodingObj(T value, String original) {
         this.value = value;
         this.original = original;
+    }
+
+    public String toString() {
+        return value.toString();
     }
 }
